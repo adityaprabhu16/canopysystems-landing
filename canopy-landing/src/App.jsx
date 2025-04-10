b4d2ea8004c07c6b6c688de2ac331b094e106f8e
Aditya Prabhu <90486414+adityaprabhu16@users.noreply.github.com>
2025-04-09 23:15:36 -0500

    initialized tailwind and vite project

---
+++ canopy-landing/src/App.jsx
@@ -0,0 +1,19 @@
+import React from 'react'
+
+const App = () => {
+  return (
+    <div>
+      <div className="container bg-red-500 mx-auto">
+        <h1 className="text-8xl text-sky-500">Hello tailwindcssv4 and react</h1>
+        <div>
+          <h2 className="text-3xl">This is 3xl</h2>
+        </div>
+        <div>
+          <button className="bg-amber-500 px-5 py-2 rounded-2xl hover:bg-amber-700">Click me!</button>
+        </div>
+      </div>
+    </div>
+  )
+}
+
+export default App
